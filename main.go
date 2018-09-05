4caf8712253d9b8a197c84791ab4229b6ecee0bf
David Schneiderbauer <dschneiderbauer@gmail.com>
2018-09-05 12:28:26 +0200

    configure logger

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"os/signal"
+	"path"
 	"syscall"
 	"time"
 
@@ -10,6 +11,7 @@ import (
 	"github.com/emed-appts/emedappts-mailer/pkg/version"
 
 	"github.com/pkg/errors"
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"gopkg.in/urfave/cli.v2"
 )
@@ -51,6 +53,38 @@ func main() {
 
 				return err
 			}
+
+			// open logfile
+			logFile, err := os.OpenFile(path.Join(config.General.Root, "mailer.log"), os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+			if err != nil {
+				log.Fatal().
+					Msgf("%+v\n", errors.Wrap(err, "could not open log file"))
+
+				return err
+			}
+			defer logFile.Close()
+
+			// configure logger
+			if config.Log.Pretty {
+				log.Logger = log.Output(
+					zerolog.ConsoleWriter{
+						Out:     logFile,
+						NoColor: !config.Log.Colored,
+					},
+				)
+			} else {
+				log.Logger = log.Output(logFile)
+			}
+
+			// set configured log level
+			logLvl, err := zerolog.ParseLevel(config.Log.Level)
+			if err != nil {
+				log.Fatal().
+					Msgf("%+v\n", errors.Wrap(err, "could not parse log level"))
+
+				return  err
+			}
+			zerolog.SetGlobalLevel(logLvl)
 
 			stop := make(chan os.Signal, 1)
 
