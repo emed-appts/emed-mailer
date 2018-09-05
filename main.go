386b4869f8f649aae7d3837066ac7c86b884d91d
David Schneiderbauer <dschneiderbauer@gmail.com>
2018-09-05 12:21:34 +0200

    project setup w/ basic cli application

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"gopkg.in/urfave/cli.v2"
+)
+
+func main() {
+	app := &cli.App{
+		Name:        "emedappt-mailer",
+		Usage:       "eMedical Appointments Mailer Service",
+		Description: "Runs the mailer service, sending notifications about booked/cancelled appointments",
+		Compiled:    time.Now(),
+
+		Authors: []*cli.Author{
+			{
+				Name:  "David Schneiderbauer",
+				Email: "david.schneiderbauer@dschneiderbauer.me",
+			},
+		},
+
+		Before: func(c *cli.Context) error {
+			return nil
+		},
+
+		Action: func(c *cli.Context) error {
+			stop := make(chan os.Signal, 1)
+
+			// todo run action
+
+			signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+			<-stop
+
+			close(stop)
+
+			return nil
+		},
+	}
+
+	cli.HelpFlag = &cli.BoolFlag{
+		Name:    "help",
+		Aliases: []string{"h"},
+		Usage:   "show the help, so what you see now",
+	}
+
+	cli.VersionFlag = &cli.BoolFlag{
+		Name:    "version",
+		Aliases: []string{"v"},
+		Usage:   "print the current version of that tool",
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		os.Exit(1)
+	}
+}
